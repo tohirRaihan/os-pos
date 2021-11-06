2e792a1242725013a87957276ad251e5db3871dd
Tohir Raihan <tohir.raihan@gmail.com>
2021-11-06 14:20:14 +0600

    profile migration completes

--- database/migrations/2021_11_06_074550_create_profiles_table.php
+++ database/migrations/2021_11_06_074550_create_profiles_table.php
@@ -15,6 +15,19 @@ class CreateProfilesTable extends Migration
     {
         Schema::create('profiles', function (Blueprint $table) {
             $table->id();
+            // foraign key
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->set('gender', ['m', 'f', 'o'])->nullable();
+            $table->string('phone', 20)->unique()->nullable();
+            $table->string('address_1', 150)->nullable();
+            $table->string('address_2', 150)->nullable();
+            $table->string('city', 50)->nullable();
+            $table->string('state', 50)->nullable();
+            $table->string('zip', 10)->nullable();
+            $table->string('country', 50)->nullable();
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
