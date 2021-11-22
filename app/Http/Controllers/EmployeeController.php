0f0c0bb8bb1db4cf3f020638f1d188d3bd1fbb5b
Tohir Raihan <tohir.raihan@gmail.com>
2021-11-22 19:38:59 +0600

    EmployeeController store method completes

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\EmployeeRequest;
 use App\Models\Employee;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -32,12 +33,31 @@ class EmployeeController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Http\Requests\EmployeeRequest  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(EmployeeRequest $request)
     {
-        //
+        // Create a user data in user table as employee
+        $user = User::create($request->all() + ['role' => 2]);
+
+        /*
+        |------------------------------------------------------------------
+        | Upload profile image inside upload folder
+        | Set image name to save it in database for later usage
+        |------------------------------------------------------------------
+        */
+        $image_name = NULL;
+        if ($image = $request->image) {
+            $image_name = $user->id . "_" . time() . "_" . rand(100, 500) . "." . $image->getClientOriginalExtension();
+            $image->move(public_path('upload/images/profile_images/'), $image_name);
+        }
+
+        // Save data in profiles and employees table
+        $user->profile()->create(array_merge($request->all(), ['image' => $image_name]));
+        $user->employee()->create($request->all());
+
+        return redirect('employees')->with('success', 'Employee created successfully!');
     }
 
     /**
