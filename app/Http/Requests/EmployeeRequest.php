1aa14a468041d9de93fdf447f317c2faa21b6691
Tohir Raihan <tohir.raihan@gmail.com>
2021-11-22 20:41:10 +0600

    EmployeeRequest added

--- app/Http/Requests/EmployeeRequest.php
+++ app/Http/Requests/EmployeeRequest.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace App\Http\Requests\Customer;
+namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Auth;
 
-class CustomerRequest extends FormRequest
+class EmployeeRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -26,8 +26,9 @@ class CustomerRequest extends FormRequest
     {
         return [
             'name' => 'required',
-            'email' => 'required',
-            'password' => 'required'
+            'email' => 'required|email',
+            'password' => 'required',
+            'employee_id' => 'unique:employees'
         ];
     }
 }
