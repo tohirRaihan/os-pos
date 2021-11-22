2f80a435934cee3f90d977078a95b75a6807eeb2
Tohir Raihan <tohir.raihan@gmail.com>
2021-11-22 20:52:49 +0600

    edit blade added in employees

---
+++ resources/views/employees/edit.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.dashboard')
+
+@section('content-title')
+    Update Employee
+@endsection
+
+@section('content')
+
+    {{-- Showing all errors on form validation --}}
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    <a href="{{ url()->previous() }}" class="btn btn-sm btn-dark btn-flat mb-3">
+        <i class="fas fa-chevron-left"></i>
+        Back
+    </a>
+
+    <!-- form start -->
+    {!! Form::model($employee, ['method' => 'PUT', 'route' => ['employees.update', $employee['id']]]) !!}
+    @include('employees.form')
+
+    <button type="submit" class="btn btn-flat btn-success d-block ml-auto px-5">Update</button>
+    {!! Form::close() !!}
+@endsection
