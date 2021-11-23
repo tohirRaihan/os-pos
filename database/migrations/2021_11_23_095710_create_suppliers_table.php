833e666995707a188b7f84df6e9c13b04190768a
Tohir Raihan <tohir.raihan@gmail.com>
2021-11-23 16:10:43 +0600

    suppliers migration file completes

--- database/migrations/2021_11_23_095710_create_suppliers_table.php
+++ database/migrations/2021_11_23_095710_create_suppliers_table.php
@@ -15,6 +15,20 @@ class CreateSuppliersTable extends Migration
     {
         Schema::create('suppliers', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('company_name');
+            $table->string('agency_name');
+            $table->set('gender', ['m', 'f', 'o'])->nullable();
+            $table->string('account_number');
+            $table->string('phone', 15);
+            $table->string('email', 60)->nullable();
+            $table->string('address_1', 150)->nullable();
+            $table->string('address_2', 150)->nullable();
+            $table->string('city', 50)->nullable();
+            $table->string('state', 50)->nullable();
+            $table->string('zip', 10)->nullable();
+            $table->string('country', 50)->nullable();
+            $table->string('note')->nullable();
             $table->timestamps();
         });
     }
