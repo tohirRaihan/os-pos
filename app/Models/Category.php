57176d45f9b69aa9b48b50483d7339dfc4e28338
Tohir Raihan <tohir.raihan@gmail.com>
2021-11-28 22:45:02 +0600

    one to many relation added between category and subcateegory

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name', 'parent_id', 'description'];
+
+    /*
+    |----------------------------------------------------------------------
+    | One to many relation with Category Model
+    |----------------------------------------------------------------------
+    */
+    public function subcategory()
+    {
+        return $this->hasMany(Category::class, 'parent_id');
+    }
+
+    public function parent()
+    {
+        return $this->belongsTo(Category::class, 'parent_id');
+    }
 }
